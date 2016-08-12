304e5feb706112e63d3d7f3ac2934c8c9d2092c2
cchiorov <cchiorov@bluewin.ch>
2016-08-12 11:38:07 +0200

    check.js has been added

--- build/js/check.js
+++ build/js/check.js
@@ -1,3 +1,33 @@
-function getMessage(a,b) {
-  return "Hello World";
+ function getMessage(a, b) {
+    if (typeof(a) === "boolean") {
+        if (a) {
+            return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
+        } else {
+            return "Переданное GIF-изображение не анимировано";
+        }
+
+    }
+    if (typeof(a) === "number") {
+        return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
+
+    }
+
+    if (Array.isArray(a) && Array.isArray(b)) {
+        var artifactSquare = 0;
+        for (var i = 0; i < a.length; i++) {
+            artifactSquare = artifactSquare + a[i] * b[i];
+        }
+        return "Общая площадь артефактов сжатия: " + artifactSquare + "  пикселей";
+
+    }
+
+    if (Array.isArray(a)) {
+        var amountOfRedPoints = 0;
+        for (var i = 0; i < a.length; i++) {
+            amountOfRedPoints = amountOfRedPoints + a[i];
+        }
+        return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+    }
+
+    return "Hello World";
 }
